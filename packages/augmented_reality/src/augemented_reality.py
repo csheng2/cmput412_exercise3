d6cb75ded45c12fe0cbdf8739068e307c85fa1b2
csheng2 <42281621+csheng2@users.noreply.github.com>
2023-02-21 13:55:27 -0700

    add function readYamlFile()

--- packages/augmented_reality/src/augemented_reality.py
+++ packages/augmented_reality/src/augemented_reality.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-import os
+import yaml
 import rospy
 from duckietown.dtros import DTROS, NodeType
-from std_msgs.msg import String
 
 '''
 Basic code for a node was taken from
@@ -14,6 +13,22 @@ class AugmentedRealityNode(DTROS):
   def __init__(self, node_name):
     # initialize the DTROS parent class
     super(AugmentedRealityNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
+
+  def readYamlFile(self,fname):
+    """
+    Reads the YAML file in the path specified by 'fname'.
+    E.G. :
+        the calibration file is located in : `/data/config/calibrations/filename/DUCKIEBOT_NAME.yaml`
+    """
+    with open(fname, 'r') as in_file:
+      try:
+        yaml_dict = yaml.load(in_file)
+        return yaml_dict
+      except yaml.YAMLError as exc:
+        rospy.loginfo("YAML syntax error. File: %s fname. Exc: %s"
+          %(fname, exc), type='fatal')
+        rospy.signal_shutdown()
+        return
 
 if __name__ == '__main__':
   # create the node
